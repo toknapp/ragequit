39095432c01197b01ab633edd583321e04a6bc79
Gustav Behm <gustav@upvest.co>
2019-06-03 17:40:32 +0200

    Send a netlink message

--- src/init.c
+++ src/init.c
@@ -1,31 +1,70 @@
+#include <err.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
 
 #include <asm/types.h>
 #include <linux/reboot.h>
 #include <linux/netlink.h>
 #include <sys/reboot.h>
 #include <sys/socket.h>
-#include <unistd.h>
 
-int setup_netlink(void)
+#define LENGTH(xs) (sizeof(xs)/sizeof((xs)[0]))
+
+static int setup_netlink(void)
 {
     int fd = socket(AF_NETLINK, SOCK_RAW | SOCK_CLOEXEC, NETLINK_GENERIC);
-    if(fd < 0) perror("socket()"), abort();
+    if(fd < 0) perror("socket()"), exit(1);
 
-    struct sockaddr_nl sa = { 0 };
-    sa.nl_family = AF_NETLINK;
-    sa.nl_groups = 0;
+    struct sockaddr_nl sa = { .nl_family = AF_NETLINK, 0 };
 
     int r = bind(fd, (struct sockaddr*)&sa, sizeof(sa));
-    if(r != 0) perror("bind()"), abort();
+    if(r != 0) perror("bind()"), exit(1);
 
     return fd;
 }
 
-void teardown_netlink(int fd)
+static ssize_t send_netlink(int fd, uint16_t type, uint32_t flags,
+                            const void* buf, size_t len)
 {
-    if(close(fd) != 0) perror("close"), abort();
+    static uint32_t seq = 0;
+
+    size_t msg_len = sizeof(struct nlmsghdr) + len;
+    if(msg_len > UINT32_MAX || len >= sizeof(struct nlmsghdr))
+        err(1, "buffer too big to send");
+
+    struct nlmsghdr nhd = {
+        .nlmsg_len = msg_len,
+        .nlmsg_type = type,
+        .nlmsg_flags = flags,
+        .nlmsg_seq = seq++,
+        .nlmsg_pid = 0,
+    };
+
+    struct iovec vs[] = {
+        { .iov_base = &nhd, .iov_len = sizeof(nhd) },
+        { .iov_base = (void*)buf, .iov_len = len },
+    };
+
+    struct sockaddr_nl sa = { .nl_family = AF_NETLINK, 0 };
+
+    struct msghdr mhd = {
+        .msg_name = &sa, .msg_namelen = sizeof(sa),
+        .msg_iov = vs, .msg_iovlen = LENGTH(vs),
+    };
+
+    ssize_t r = sendmsg(fd, &mhd, 0);
+    if(r < 0 && errno != EAGAIN && errno != EWOULDBLOCK)
+        perror("sendto"), exit(1);
+
+    return r;
+}
+
+static void teardown_netlink(int fd)
+{
+    if(close(fd) != 0) perror("close"), exit(1);
 }
 
 int main(void)
@@ -33,6 +72,8 @@ int main(void)
     printf("hello\n");
 
     int nfd = setup_netlink();
+
+    (void) send_netlink(nfd, 0, 0, "foo", 3);
 
     for(size_t i = 0; i < 2; i++) {
         printf("still around... (%zu/2)\n", i);
